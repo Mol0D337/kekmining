37cd21829d1a860bd9119f6942245a9f8ebe43b4
Mol0D337 <zmolodyk@gmail.com>
2020-12-01 18:34:20 +0200

    add button style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,70 @@
 <template>
   <div class="home">
-    <p>1dqwрарарарer4</p>
+    <button class="btn btn-r" >
+      <span>Registration</span>
+    </button>
+
+    <button class="btn btn-l" >
+      <span>Login</span>
+    </button>
+
+    <button class="btn btn-card" >
+      <span>Get started</span>
+    </button>
+
+    <button class="btn btn-card btn-main" >
+      <span>Get started</span>
+    </button>
+
+    <button class="btn btn-menu-color" >
+      <span>Вывод</span>
+    </button>
+
+    <button class="btn btn-menu-border" >
+      <span>Реинвестировать</span>
+    </button>
+
+    <button class="btn btn-table-1" >
+      <span>Кнопка 1</span>
+    </button>
+
+    <button class="btn btn-table-2" >
+      <span>Кнопка 2</span>
+    </button>
+
+    <button class="btn btn-cycle" >
+      <span>
+        <
+      </span>
+    </button>
+
+    <button class="btn btn-cycle-num" >
+      <span>01</span>
+    </button>
+
+    <button class="btn btn-cycle" >
+      <span>
+        >
+      </span>
+    </button>
+
+
+
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'Home',
   components: {
   }
 }
 </script>
+
+<style  scoped lang="sass">
+  .btn
+
+
+
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -33,9 +33,6 @@ module.exports = {
       }
     }
   },
-  transpileDependencies: [
-    'vuetify'
-  ],
 
   chainWebpack: config => {
     config.module
