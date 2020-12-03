c31540034cf49a26c48366a043d2dce286a4010f
Mol0D337 <zmolodyk@gmail.com>
2020-12-03 23:57:44 +0200

    login in main page

---
+++ src/components/Product/Product.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="product">
+    <img src="../../assets/images/imgIcons/18.png" alt="">
+    <div class="product__title"><b>Bitcoin</b></div>
+    <div class="product__min">Minimal hashrate: <b>500 Gh/s</b></div>
+    <div class="product__pay">Payment for maintenance: <br/><b>0.0010000000 / 1 Gh/s / 1h</b></div>
+    <div class="product__equipment">Equipment: <b>SHA-256</b></div>
+    <input type="range" min="0" max="100" step="1" value="0">
+    <div class="product__price"><b>$250</b></div>
+    <button class="btn btn-card">
+      <span>Get started</span>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Product",
+    components: {},
+    props: {
+      product_data: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+    },
+  }
+</script>
+
+<style scoped lang="sass">
+  .product
+    margin: 30px
+    text-align: center
+    background: $color-white
+    width: 300px
+    box-shadow: 0 0 30px rgba(0, 0, 0, 0.05)
+    +reg
+    font-size: 15px
+    line-height: 1.5
+
+    &:hover
+      box-shadow: 0 0 30px rgba(0, 0, 0, 0.1)
+    img
+      margin-top: 15px
+      margin-bottom: 10px
+    &__title
+      font-size: 20px
+    &__min
+      margin-top: 10px
+      margin-bottom: 10px
+    &__pay
+      margin-top: 10px
+      margin-bottom: 10px
+    &__equipment
+      margin-bottom: 30px
+    &__price
+      margin-top: 20px
+      margin-bottom: 15px
+      font-size: 20px
+
+  .btn
+    margin-bottom: 20px
+</style>
--- src/views/MainPage/MainPage.vue
+++ src/views/MainPage/MainPage.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <div class="opacity"></div>
     <div class="all">
       <div class="wrapper">
         <div class="wrapper__header">
@@ -79,6 +78,47 @@
 
       </div>
 
+      <div class="wrapper__video">
+        <div class="video">
+          <div class="possibilities__title">
+            Video title
+          </div>
+          <iframe
+            width="800" height="430"
+            src="https://www.youtube-nocookie.com/embed/xhzNt4xUd24"
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write;
+            encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+          </iframe>
+        </div>
+      </div>
+
+      <div class="bcg">
+        <div class="wrapper__about">
+          <div class="about">
+            <div class="about__title">
+              About us
+            </div>
+            <div class="about__subtitle">
+              Why us?
+            </div>
+            <div class="about__text">
+              Kekmining is a modern approach, the most innovative systems and the maximum ease of use.
+              Now speaking about mining, you should not think about buying and setting up expensive
+              equipment, it's enough to choose the necessary speed, to distribute assets and get the
+              maximum income.
+            </div>
+          </div>
+        </div>
+        <div class="grid">
+          <Product
+            v-for="product in products"
+            :key="product.article"
+            :product_data="product"
+          />
+        </div>
+      </div>
+
     </div>
   </div>
 
@@ -89,25 +129,102 @@
 import Login from '../../components/Login/Login'
 import Registration from "../../components/Login/Registration";
 import SvgIcon from "../../elements/SvgIcon";
+import Product from "../../components/Product/Product";
 export default {
   name: 'MainPage',
   components: {
+    Product,
     SvgIcon,
     Registration,
     Login
   },
+  props: {},
+  data () {
+    return {
+      products: [
+        {
+          image: "18.png",
+          name: "Bitcoin",
+          min: "500 Gh/s",
+          pay: "0.0010000000 / 1 Gh/s / 1h",
+          equipment: "SHA-256",
+          price: "$250",
+          article: "T1",
+        },
+        {
+          image: "17.png",
+          name: "Dash",
+          min: "1150 Mh/s",
+          pay: "0.0010000000 / 1 Mh/s / 1h",
+          equipment: "X11 (DASH)",
+          price: "$450",
+          article: "T2",
+          available: true
+        },
+        {
+          image: "16.png",
+          name: "Ethereum",
+          min: "50 Mh/s",
+          pay: "0.0010000000 / 1 Mh/s / 1h",
+          equipment: "ETHASH",
+          price: "$20",
+          article: "T3",
+          available: true
+        },
+        {
+          image: "19.png",
+          name: "Litecoin",
+          min: "2500 Mh/s",
+          pay: "0.0010000000 / 1 Mh/s / 1h",
+          equipment: "Scrypt",
+          price: "$2500",
+          article: "T4",
+          available: true
+        },
+        {
+          image: "20.png",
+          name: "Zcash",
+          min: "1000 H/s",
+          pay: "0.0010000000 / 1 H/s / 1h",
+          equipment: "EQUEHASH (ZEC)",
+          price: "$60",
+          article: "T5",
+          available: true
+        },
+        {
+          image: "21.png",
+          name: "Monero",
+          min: "25 H/s",
+          pay: "0.0000571667 / 1 H/s / 1h",
+          equipment: "Cryptonight (XMR)",
+          price: "$13",
+          article: "T6",
+          available: true
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped lang="sass">
+  .grid
+    display: grid
+    grid-template-columns: 1fr 1fr 1fr
+    grid-template-rows: 1fr 1fr
+  .bcg
+    background-image: url('../../assets/images/backgroundD/bcg.svg')
+    background-position: 0 0
+    background-repeat: no-repeat
+    background-size: cover
+    background-attachment: local
+
   .btn-main
     box-shadow: 0 0 30px rgba(255, 255, 255, 0.25)
 
   .all
-    margin-bottom: 1000px
-
     width: 100vw
-    height: 100vh
+    height: 107vh
     background: url("../../assets/images/imgPage/111.png") no-repeat center center
     background-size: cover
     transition: background-position .5s ease-out
@@ -181,7 +298,7 @@ export default {
     width: 600px
 
   .possibilities
-    margin-top: 235px
+    margin-top: 300px
     text-align: center
     &__title
       text-align: center
@@ -240,5 +357,15 @@ export default {
     color: $color-black
     font-size: 16px
 
+  .wrapper__video
+    margin-top: 50px
+    margin: 0 auto
+    width: 800px
 
+  .video
+    margin-top: 80px
+
+  iframe
+    margin-top: 40px
+    margin-bottom: 40px
 </style>
