a7218f8f40ea274f7e7698f28c5c9883f2ec5287
Mol0D337 <zmolodyk@gmail.com>
2020-12-09 23:47:11 +0200

    add LF-help

--- src/views/AdminPanel/Help.vue
+++ src/views/AdminPanel/Help.vue
@@ -4,18 +4,114 @@
 
     <div class="wrapper__help">
 
-      <DropDown
-      :title="$t('any.codes')"
-      :initial-value="true"
-    >
-      <div>{{$t('form.hello')}}</div>
-    </DropDown>
+      <div class="text"><b>{{$t('any.help')}}</b></div>
 
-      <DropDown
-        :title="$t('any.codes')"
-      >
-        <div>12414124</div>
-      </DropDown>
+      <div class="wrapper__help-grid">
+        <div class="wrapper__help-grid-l">
+          <DropDown
+            :title="$t('questions.question1')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question2')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question3')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question4')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question5')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question6')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question7')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question8')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question9')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question10')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+        </div>
+        <div class="wrapper__help-grid-l">
+          <DropDown
+            :title="$t('questions.question11')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question12')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question13')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question14')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question15')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question16')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question17')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question18')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question19')"
+          >
+            <div class="padding">{{$t('answers.answer1')}}</div>
+          </DropDown>
+          <DropDown
+            :title="$t('questions.question20')"
+          >
+            <div class="padding">{{$t('answers.answer2')}}</div>
+          </DropDown>
+        </div>
+      </div>
 
     </div>
 
@@ -34,11 +130,29 @@
 </script>
 
 <style scoped lang="sass">
+  .padding
+    padding: 15px
+
   .bcg
     background: $color-grey
 
   .wrapper__help
-    width: 1210px
     margin: 0 auto
+    width: 1250px
+    padding: 30px 0 80px 0
+
+    &-grid
+      display: grid
+      grid-template-columns: 1fr 1fr
+
+      &-l
+        line-height: 1.5
+        width: 100%
+        height: 100%
+
+  .text
+    padding: 0 0 40px 20px
+    +reg
+    font-size: 34px
 
 </style>
