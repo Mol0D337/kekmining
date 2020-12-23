1e3b4259ff07c66418360d85be778f625335300a
Mol0D337 <zmolodyk@gmail.com>
2020-12-23 22:55:16 +0200

    history

--- src/views/AdminPanel/History.vue
+++ src/views/AdminPanel/History.vue
@@ -2,10 +2,21 @@
   <div class="bcg">
     <NavBar/>
 
-    <div style="padding-bottom: 1000px">
-      <label>English DatePicker :</label>
+    <div class="wrapper__content">
+      <div class="text"><b>{{$t('any.referrals')}}</b></div>
+
+      <DropDown
+        :title="$t('any.codes')"
+        :initial-value="true"
+      >
+        <button style="margin: 20px 0 20px 17px" class="btn btn-table-1">
+          <span>{{$t('table.addNewCode')}}</span>
+        </button>
+
+        <Table/>
 
 
+      </DropDown>
     </div>
 
     <ToolBar/>
@@ -14,9 +25,10 @@
 <script>
   import NavBar from "../../components/MyAdminPanel/NavBar";
   import ToolBar from "../../components/MyAdminPanel/ToolBar";
+  import DropDown from "../../components/DropDown/DropDown";
   export default {
     name: "History",
-    components: {ToolBar, NavBar},
+    components: {DropDown, ToolBar, NavBar},
     data() {
       return {
         date: ''
@@ -28,5 +40,14 @@
 <style scoped lang="sass">
   .bcg
     background: $color-grey
+
+  .wrapper__content
+    margin: 0 auto
+    width: 1250px
+
+  .text
+    padding: 30px 0 10px 20px
+    +reg
+    font-size: 34px
 
 </style>
